7d8bcf55944321ff16a530a94cfc9f7dd16e16f9
Rubin Rastogi <rubin.rastogi@mail.utoronto.ca>
2022-10-09 07:08:30 -0400

    Did the TODOs. Yay!!!

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -80,6 +80,17 @@ public class Silly implements Comparable<Silly>{
      *       Make sure you document this method!
      */
 
+    /** Creates a new Silly object.
+     * This parameter takes in two strings as arguments.
+     *
+     * @param s1 is the first string.
+     * @param s2 is the second string.
+     * This Silly instance's name is the concatenation of s1 and s2.
+     */
+    public Silly (String s1, String s2) {
+        this.name = s1 + s2;
+    }
+
 
 
 
@@ -116,7 +127,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +145,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -161,6 +173,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return this.name.equals(other.name);
     }
 
     /**
@@ -179,7 +192,7 @@ public class Silly implements Comparable<Silly>{
      * Returns a negative integer, zero, or a positive integer as this
      * object is less than, equal to, or greater than the specified object.
      *
-     * @param other the object to be compared.
+     * @param other the Silly object to be compared.
      * @return a negative integer, zero, or a positive integer as this
      * object is less than, equal to, or greater than the specified object.
      */
@@ -194,6 +207,7 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        return (this.name.length() - other.name.length());
     }
 
     /*
